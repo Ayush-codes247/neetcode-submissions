99a045995ad4dca31042e6c2712a4693116b9c5b
Ayush Singh <ayush11592401@gmail.com>
2026-07-09 19:34:53 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        int i = 0;
+        vector<int> ans;
+        for(i = 0; i < nums.size(); i++){
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(dq.front() < i - k + 1){
+                dq.pop_front();
+            }
+            if(i >= k - 1){
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
